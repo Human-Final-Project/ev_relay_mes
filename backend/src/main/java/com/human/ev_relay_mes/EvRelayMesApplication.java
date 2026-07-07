fe3f0bfcbe23cf1a87a73b4104c0f60676ab9902
JunHee <cyanaria89@gmail.com>
2026-07-07 15:03:39 +0900

    프로젝트 기본 파일

---
+++ backend/src/main/java/com/human/ev_relay_mes/EvRelayMesApplication.java
@@ -0,0 +1,13 @@
+package com.human.ev_relay_mes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvRelayMesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvRelayMesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/human/ev_relay_mes/EvRelayMesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.human.ev_relay_mes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvRelayMesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
